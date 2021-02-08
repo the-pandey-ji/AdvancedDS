2ddb9eef6a5cfde48b90b898bb3e047a1d223b8b
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2021-02-08 10:08:51 +0530

    SinglyLinkedList created and displayed

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,2 +1,36 @@
+import java.util.List;
+
 public class SinglyLinkedList {
+    private ListNode head;
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+        sll.head.next = second; //10->1
+        second.next = third;//10->1->8
+        third.next = fourth;//10->1->8->11->null
+        sll.display();
+    }
+
+    public void display() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    private static class ListNode {
+        private int data;//Generic type
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
