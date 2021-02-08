a472c68c8d6398ac9c491b19e0fdc3ea1c167065
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2021-02-08 10:17:56 +0530

    length method

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -2,7 +2,6 @@ import java.util.List;
 
 public class SinglyLinkedList {
     private ListNode head;
-
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.head = new ListNode(10);
@@ -13,8 +12,10 @@ public class SinglyLinkedList {
         second.next = third;//10->1->8
         third.next = fourth;//10->1->8->11->null
         sll.display();
+        System.out.println("Length is - " + sll.length());
     }
 
+    //Display Singly Linked List
     public void display() {
         ListNode current = head;
         while (current != null) {
@@ -22,6 +23,21 @@ public class SinglyLinkedList {
             current = current.next;
         }
         System.out.println("null");
+    }
+
+    //Calculate the length of LinkedList
+    public int length() {
+        if (head == null) {
+            return 0;
+        }
+        int count = 0;
+        ListNode current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+
+        }
+        return count;
     }
 
     private static class ListNode {
